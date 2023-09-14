881670524e52fe34b974001cb090bfa1df8f5653
Shyam Sundar Tiwari <shyampersonal2000@gmail.com>
2023-09-15 00:07:11 +0530

    added remaining pages

--- script.js
+++ script.js
@@ -24,20 +24,67 @@ songitems.forEach((element, i)=>{
     element.getElementsByClassName("songname")[0].innerText=songs[i].songname;
 })
 
+function loadHTML(className, fileName) {
+	console.log('Div id: ' + className + ', filename: ' + fileName);
+
+	let xhttp;
+	const element = document.querySelectorAll('.' + className);
+	console.log(element.length);
+	let file = fileName;
+
+	if (file) {
+		element.forEach((elem) => {
+			xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function () {
+				if (this.readyState == 4) {
+					if ((this.status = 200)) {
+						var importText = this.responseText;
+						elem.innerHTML = importText;
+						console.log(
+							'Successfully imported ' + file + ' ' + elem.id
+						);
+
+						//now running javascript of the imported file
+						const parser = new DOMParser();
+						var importDoc = this.responseText;
+						var importHTML = parser.parseFromString(
+							importDoc,
+							'text/html'
+						);
+						var importScripts =
+							importHTML.querySelectorAll('script');
+						importScripts.forEach((importScript) => {
+							var script = document.createElement('script');
+							script.textContent = importScript.textContent;
+							elem.appendChild(script);
+						});
+					} else if ((this.status = 404)) {
+						console.log('Requested file not found');
+						elem.innerHTML = '<h1>Requested file not found</h1>';
+					}
+				}
+			};
+			xhttp.open('GET', './html/' + file, true);
+			xhttp.send();
+			return;
+		});
+	}
+}
+
 //audioElement.play();
 //handle play/pause
 masterplay.addEventListener('click', ()=>{
+    masterplay.classList.remove('fa-play-circle');
+    masterplay.classList.add('fa-pause-circle');
     if(audioElement.paused|| audioElement.currentTime<=0)
     {
         audioElement.play();
-        masterplay.classList.remove('fa-play-circle');
-        masterplay.classList.add('fa-pause-circle');
         gif.style.opacity=1;
     }
     else{
         audioElement.pause();
-        masterplay.classList.remove('fa-pause-circle');
-        masterplay.classList.add('fa-play-circle');
+        // masterplay.classList.remove('fa-pause-circle');
+        // masterplay.classList.add('fa-play-circle');
         gif.style.opacity=0;
         
     }
@@ -61,8 +108,8 @@ const makeallplays=()=>{
     //e.target.classList.add('fa-circle-pause fa-xl');
     Array.from(document.getElementsByClassName('songitemplay')).forEach((element)=>{
 
-        element.classList.remove('fa-circle-pause fa-xl');
-        element.classList.add('fa-circle-play fa-xl');
+        element.classList.remove('fa-circle-pause');
+        element.classList.add('fa-circle-play');
     })
 }
 Array.from(document.getElementsByClassName('songitemplay')).forEach((element)=>{
@@ -71,16 +118,16 @@ Array.from(document.getElementsByClassName('songitemplay')).forEach((element)=>{
         makeallplays();
         songindex=parseInt(e.target.id);
         
-        e.target.classList.remove('fa-circle-play fa-xl');
-        e.target.classList.add('fa-circle-pause fa-xl');
+        e.target.classList.remove('fa-circle-play');
+        e.target.classList.add('fa-circle-pause');
         //audioElement.src ='songs/${index+1}.mp3';
         audioElement.src = `songs/${songindex+1}.mp3`;
         mastersongname.innerText=songs[songindex].songname;
         audioElement.currentTime=0;
         audioElement.play();
         gif.style.opacity=1;
-        masterplay.classList.remove('fa-circle-play fa-xl');
-        masterplay.classList.add('fa-circle-pause fa-xl');
+        masterplay.classList.remove('fa-circle-play');
+        masterplay.classList.add('fa-circle-pause');
     })
 
     
