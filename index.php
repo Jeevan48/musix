5c5581afe3da9fbc210d0a1c9237c12934020dc1
Shyam Sundar Tiwari <shyampersonal2000@gmail.com>
2023-11-06 12:07:19 +0530

    update for cc miniproject showcase

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="./css/style.css"> -->
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Varela+Round&display=swap');
 
