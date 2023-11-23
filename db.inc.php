e015fed6e3ebb6d97877a8f8d58c9ac6ec4d1da8
Shyam Tiwari <shyampersonal2000@gmail.com>
2023-11-24 01:33:33 +0530

    Updated db.inc.php

--- db.inc.php
+++ db.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_SERVER', 'musix-db-1.cvv5gqn00rpc.us-east-1.rds.amazonaws.com');
+define('DB_SERVER', 'musix-db.cvv5gqn00rpc.us-east-1.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'root1234');
 define('DB_DATABASE', 'SONGS_DATABASE');
